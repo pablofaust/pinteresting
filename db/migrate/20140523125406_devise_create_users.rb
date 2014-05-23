48e66417bcf595a68a672ef94185144c2ecb0d8b
Pablo Faust <pabfaust@gmail.com>
2014-05-23 15:42:56 +0200

    Debug

--- db/migrate/20140523125406_devise_create_users.rb
+++ db/migrate/20140523125406_devise_create_users.rb
@@ -1,6 +1,10 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
-    
+    create_table(:users) do |t|
+      ## Database authenticatable
+      t.string :email,              :null => false, :default => ""
+      t.string :encrypted_password, :null => false, :default => ""
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
